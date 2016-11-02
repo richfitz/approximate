9c4d3bf40df490efdb8713736c826ca79ab826fb
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-11-02 16:36:44 +0000

    Expand test coverage

--- tests/testthat/test-approximate.R
+++ tests/testthat/test-approximate.R
@@ -23,3 +23,34 @@ test_that("approximate", {
   err <- pmax(abs(z_mid - y_mid), abs(1 - z_mid / y_mid))
   expect_true(all(err < 1e-6))
 })
+
+test_that("non-vectorised", {
+  target <- function(x) sin(2 * x)
+  target2 <- function(x) {
+    if (length(x) != 1L) {
+      stop("I require scalar x")
+    }
+    target(x)
+  }
+
+  r <- c(0, 2 * pi)
+
+  s1 <- approximate(target, r[1], r[2], tol = 1e-6)
+  expect_error(approximate(target2, r[1], r[2], tol = 1e-6),
+               "I require scalar x")
+  s2 <- approximate(target, r[1], r[2], tol = 1e-6, target_vectorised = FALSE)
+
+  xx <- seq(r[[1]], r[[2]], length.out = 1000)
+  expect_equal(s1(xx), s2(xx))
+})
+
+test_that("error cases", {
+  target <- runif
+  expect_error(approximate(target, 1, 0), "Impossible bounds")
+  expect_error(approximate(target, 0, NA), "Bounds must be finite")
+  expect_error(approximate(target, NA, 1), "Bounds must be finite")
+  expect_error(approximate(target, 0, Inf), "Bounds must be finite")
+  expect_error(approximate(target, Inf, 1), "Bounds must be finite")
+  expect_error(approximate(target, 0, 1, max_depth = 3),
+               "function is as refined")
+})
